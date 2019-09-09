aa1b8bb5fb16ee48bf38c58f1bd2812216ef9a35
Lucas Schilperoort <398389@student.fontys.nl>
2019-09-09 10:29:38 +0200

    bruh

---
+++ Assets/webTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using UnityEngine;
+
+   public class webTest:MonoBehaviour
+    {
+    private static readonly HttpClient client = new HttpClient();
+    [SerializeField]
+    string URL;
+    private async void Start()
+    {
+        HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Head, URL);
+     HttpResponseMessage response =   await client.SendAsync(msg);
+        Debug.Log(response.Content.Headers.ContentType);
+    }
+}
